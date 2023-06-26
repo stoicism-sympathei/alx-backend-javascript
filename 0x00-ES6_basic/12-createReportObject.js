eeb7b2fe2292740075a90d7c740a19a44dd40c79
Stoicism-Sympatheia <30344545+stoicism-sympathei@users.noreply.github.com>
2023-06-26 16:32:27 -0400

    Update

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,8 +1,16 @@
 export default function createReportObject(employeesList) {
+  const allEmployees = {};
+
+  employeesList.forEach((employee) => {
+    const { name, department } = employee;
+    if (!(department in allEmployees)) {
+      allEmployees[department] = [];
+    }
+    allEmployees[department].push(name);
+  });
+
   return {
-    allEmployees: { ...employeesList },
-    getNumberOfDepartments(employeesList) {
-      return Object.keys(employeesList).length;
-    },
+    allEmployees,
+    getNumberOfDepartments: () => Object.keys(allEmployees).length,
   };
 }
