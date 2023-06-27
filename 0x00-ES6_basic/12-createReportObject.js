046eab4320a6c9bfc4c31d066870be793361c453
stoicism-sympatheia <mogesanonymous@gmail.com>
2023-06-27 16:29:20 -0400

    Let's create a report object

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,16 +1,10 @@
 export default function createReportObject(employeesList) {
-  const allEmployees = {};
-
-  employeesList.forEach((employee) => {
-    const { name, department } = employee;
-    if (!(department in allEmployees)) {
-      allEmployees[department] = [];
-    }
-    allEmployees[department].push(name);
-  });
-
   return {
-    allEmployees,
-    getNumberOfDepartments: () => Object.keys(allEmployees).length,
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
   };
 }
