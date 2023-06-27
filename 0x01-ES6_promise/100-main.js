391bcfec4080d916b96ad14c9e23a5f28dbe2db9
stoicism-sympatheia <mogesanonymous@gmail.com>
2023-06-27 11:42:12 -0400

    Await / Async

---
+++ 0x01-ES6_promise/100-main.js
@@ -0,0 +1,18 @@
+import { uploadPhoto, createUser } from "./utils";
+
+async function asyncUploadUser() {
+  try {
+    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
+    return {
+      photo,
+      user,
+    };
+  } catch (error) {
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}
+
+export default asyncUploadUser;
