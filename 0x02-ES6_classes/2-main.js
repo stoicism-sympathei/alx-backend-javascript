6b31c73efd0ef077f66d70570234d976679aa859
stoicism-sympatheia <mogesanonymous@gmail.com>
2023-06-29 08:18:33 -0400

    Methods, static methods, computed methods names..... MONEY

--- 0x02-ES6_classes/2-main.js
+++ 0x02-ES6_classes/2-main.js
@@ -1,15 +1,24 @@
 import HolbertonCourse from "./2-hbtn_course.js";
 
-const c1 = new HolbertonCourse("ES6", 1, ["Bob", "Jane"])
-console.log(c1.name);
+const c1 = new HolbertonCourse("ES6", 1, ["Bob", "Jane"]);
+console.log(c1.name); // Output: ES6
+
 c1.name = "Python 101";
 console.log(c1);
+/* Output:
+HolbertonCourse {
+  _name: 'Python 101',
+  _length: 1,
+  _students: [ 'Bob', 'Jane' ]
+}
+*/
 
 try {
     c1.name = 12;
 } 
 catch(err) {
     console.log(err);
+    // Output: TypeError: Name must be a string
 }
 
 try {
@@ -17,4 +26,6 @@ try {
 }
 catch(err) {
     console.log(err);
+    // Output: TypeError: Length must be a number
 }
+
