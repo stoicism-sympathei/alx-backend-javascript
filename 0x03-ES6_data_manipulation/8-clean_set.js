7d186a7499c23b151bfe0c94f528f4c1d7e046d3
stoicism-sympatheia <mogesanonymous@gmail.com>
2023-07-03 12:39:15 -0400

    Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,11 @@
-export default function cleanSet(set, startString) {
-  if (!startString || typeof startString !== 'string' || typeof set !== 'object') return '';
-  return [...set]
-    .filter((el) => el.startsWith(startString))
-    .map((el) => el.slice(startString.length))
-    .join('-');
+export default function cleanSet(_set, startString) {
+  if (typeof _set !== 'object') return '';
+  if (typeof startString !== 'string') return '';
+  if (startString.length === 0) return '';
+
+  const stringSet = [];
+  [..._set].forEach((x) => {
+    if (x && x.indexOf(startString) === 0) stringSet.push(x.replace(startString, ''));
+  });
+  return stringSet.join('-');
 }
