1c782d231495986f45c3eab2d8f9c31b7cbf2c5b
stoicim-sympatheia <mogesanonymous@gmail.com>
2023-08-22 12:45:13 -0400

    fixed

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,12 +1,13 @@
 const http = require('http');
 const readStudentsData = require('./3-read_file_async');
+
 const hostname = '127.0.0.1';
 const port = 1245;
 
 const app = http.createServer(async (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  
+
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
@@ -19,7 +20,7 @@ const app = http.createServer(async (req, res) => {
         'This is the list of our students',
         `Number of students: ${studentsData.students.length}`,
         `Number of students in CS: ${csStudents.length}. List: ${csStudents}`,
-        `Number of students in SWE: ${sweStudents.length}. List: ${sweStudents}`
+        `Number of students in SWE: ${sweStudents.length}. List: ${sweStudents}`,
       ].join('\n');
 
       res.end(response);
