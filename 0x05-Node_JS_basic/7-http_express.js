12ed33f8d25bf25b35291a6daf227ec38f93c785
stoicim-sympatheia <mogesanonymous@gmail.com>
2023-08-25 05:24:32 -0400

    #7

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -2,30 +2,34 @@ const express = require('express');
 const students = require('./3-read_file_async');
 
 const app = express();
+const hostname = '127.0.0.1';
 const port = 1245;
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello Holberton School!\n');
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.send('Hello Holberton School!');
 });
 
 app.get('/students', async (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.write('This is the list of our students\n');
+
   try {
     const data = await students(process.argv[2]);
-    const response = `
-  This is the list of our students
-  Number of students: ${data.students.length}
-  Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}
-  Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}
-`;
-
-    res.status(200).send(response);
+    res.write(`Number of students: ${data.students.length}\n`);
+    res.write(`Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`);
+    res.write(`Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}`);
   } catch (err) {
-    res.status(500).send(err.message);
+    res.write(err.message);
+  } finally {
+    res.end();
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
 });
 
-module.exports = app; // Export the Express app
+module.exports = app;
