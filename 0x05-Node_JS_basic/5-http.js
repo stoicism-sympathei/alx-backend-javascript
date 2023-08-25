54150e7217970a4c71df319033092da5b647e319
stoicim-sympatheia <mogesanonymous@gmail.com>
2023-08-25 04:54:20 -0400

    #5

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,37 +1,37 @@
 const http = require('http');
-const readStudentsData = require('./3-read_file_async');
+const fs = require('fs');
 
-const hostname = '127.0.0.1';
-const port = 1245;
+const app = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/plain');
 
-const app = http.createServer(async (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+    if (req.url === '/') {
+        res.end('Hello Holberton School!\n');
+    } else if (req.url === '/students') {
+        // Replace 'students.csv' with the actual CSV file name
+        const csvFileName = 'students.csv';
+        
+        // Replace 'database_name' with the actual database name
+        const dbName = 'database_name';
+        
+        fs.readFile(csvFileName, 'utf-8', (err, data) => {
+            if (err) {
+                res.statusCode = 500;
+                res.end('Internal Server Error');
+            } else {
+                const lines = data.split('\n');
+                const validStudents = lines.filter(line => line.trim() !== '');
 
-  if (req.url === '/') {
-    res.end('Hello Holberton School!');
-  } else if (req.url === '/students') {
-    try {
-      const studentsData = await readStudentsData(process.argv[2]);
-      const csStudents = studentsData.csStudents.join(', ');
-      const sweStudents = studentsData.sweStudents.join(', ');
-
-      const response = [
-        'This is the list of our students',
-        `Number of students: ${studentsData.students.length}`,
-        `Number of students in CS: ${csStudents.length}. List: ${csStudents}`,
-        `Number of students in SWE: ${sweStudents.length}. List: ${sweStudents}`,
-      ].join('\n');
-
-      res.end(response);
-    } catch (error) {
-      res.end(error.message);
+                res.end(`This is the list of our students:\n${validStudents.join('\n')}`);
+            }
+        });
+    } else {
+        res.statusCode = 404;
+        res.end('Not Found');
     }
-  }
 });
 
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}`);
+app.listen(1245, () => {
+    console.log('Server is listening on port 1245');
 });
 
 module.exports = app;
